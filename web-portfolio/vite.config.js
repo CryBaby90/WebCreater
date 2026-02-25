024b9bfe28af9d223b31c92dd80f6c09b7c316ab
huya <huya@wooduan.com>
2026-02-25 14:26:37 +0800

    Update vite config

--- web-portfolio/vite.config.js
+++ web-portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WebCreater/', // GitHub Pages 路径
+  base: '/', // GitHub Pages 路径（仓库名已在 URL 中）
 })
